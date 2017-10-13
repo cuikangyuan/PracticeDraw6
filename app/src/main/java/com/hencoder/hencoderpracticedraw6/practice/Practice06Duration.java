cfbae11f855b62ad4fa26982a7058db8d70aabd7
cuikangyuan <cuikangyuan@outlook.com>
2017-10-13 15:01:20 +0800

    添加练习代码

--- app/src/main/java/com/hencoder/hencoderpracticedraw6/practice/Practice06Duration.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw6/practice/Practice06Duration.java
@@ -11,6 +11,7 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.hencoder.hencoderpracticedraw6.R;
+import com.hencoder.hencoderpracticedraw6.Utils;
 
 public class Practice06Duration extends LinearLayout {
     SeekBar durationSb;
@@ -68,7 +69,7 @@ public class Practice06Duration extends LinearLayout {
                 // TODO 在这里处理点击事件，执行动画。记得使用 `setDuration(duration)` 来设置动画的时长。
                 switch (state) {
                     case 0:
-                        imageView.animate().translationX(200).setDuration(duration);
+                        imageView.animate().translationX(Utils.dpToPixel(100)).setDuration(duration);
                         break;
                     case 1:
                         imageView.animate().translationX(0).setDuration(duration);
